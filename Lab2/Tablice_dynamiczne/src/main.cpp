3916549ce6f27a6e21aa0f3af3e84c4159d73321
lukaszbrzeszcz <226362@student.pwr.wroc.pl>
2017-03-15 23:47:05 +0100

    Poprawki estetyczne

--- Lab2/Tablice_dynamiczne/src/main.cpp
+++ Lab2/Tablice_dynamiczne/src/main.cpp
@@ -12,21 +12,10 @@ int main() {
   int blad = 0;
   clock_t start, stop;
   double czas;
-
-  /*
-  cout << "Podaj rozmiar tablicy: " << endl;
-  cin >> rozmiar;
-  Tablica tablica(rozmiar);
-  cout << "Podaj pozycje:  " << endl;
-  cin >> pozycja;
-  cout << "Podaj wartosc: " << endl;
-  cin >> wartosc;
-  tablica.przypisz(pozycja, wartosc);
-  tablica.odczytaj(pozycja, wartosc1);  
-  */
   Tablica tab(ROZMIAR);
 
   start = clock();
+  
   for (int i=0; i<MAX; i++) {
     tab.przypisz(i, 1, blad);
   }
--- Lab2/Tablice_dynamiczne/src/tablica.cpp
+++ Lab2/Tablice_dynamiczne/src/tablica.cpp
@@ -9,26 +9,30 @@ using namespace std;
 
 Tablica::Tablica() {
   
-  _rozmiar = 1;
+   _rozmiar = 1;
   _wypelnienie = 0;
   _Tablica = new int[_rozmiar];
   _Tablica[0] = 0;
-
+  
 }
+
+
 
 Tablica::~Tablica() {
   delete [] _Tablica;
 }
 
 
-Tablica::Tablica(int rozmiar) {
 
+Tablica::Tablica(int rozmiar) {
   _rozmiar = rozmiar;
   _Tablica = new int[_rozmiar];
+  
   for (int i=0; i<rozmiar; i++) {
     _Tablica[i] = 0;
   }
   _wypelnienie = 0;
+  
 }
 
 
@@ -45,16 +49,12 @@ void Tablica::przypisz(int pozycja, int wartosc, int &blad) {
 
 
 void Tablica::odczytaj(int pozycja, int &wartosc) {
-
   wartosc = _Tablica[pozycja];
-  
 }
 
 
 void Tablica::pokaz_rozmiar(int &rozmiar) {
-
   rozmiar = _rozmiar;
-
 }
 
 void Tablica::powieksz(int tryb) {
@@ -65,6 +65,7 @@ void Tablica::powieksz(int tryb) {
     for(int i=0; i<_rozmiar; i++) {
       bufor[i] = _Tablica[i];
     }
+    
     _rozmiar = 2*_rozmiar;
     delete [] _Tablica;
     _Tablica = &bufor[0];
@@ -76,6 +77,7 @@ void Tablica::powieksz(int tryb) {
     for (int i=0; i<_rozmiar; i++) {
       bufor[i] = _Tablica[i];
     }
+    
     _rozmiar++;
     delete [] _Tablica;
     _Tablica = &bufor[0];
